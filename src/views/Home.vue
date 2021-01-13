f62a9a64356103e2b6b49806e3fe5198c7a5a42f
Adis Subasic <adsubasic@gmail.com>
2021-01-13 10:57:50 +0100

    inital commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <img alt="Vue logo" src="../assets/sova.jpg">
+    <HelloWorld msg="Welcome to our Istructions app"/>
   </div>
 </template>
 
